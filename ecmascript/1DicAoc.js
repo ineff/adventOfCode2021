5f60411aa4f5ba03cb274019cdc0bc9f4740cc1b
Giorgio Mossa <giorgio.mossa88@gmail.com>
2021-12-02 18:43:02 +0100

    Initialized a repo for the solution to the advent of code of 2021.

---
+++ ecmascript/1DicAoc.js
@@ -0,0 +1,107 @@
+/*
+* Solutions for the first day of the advent of Code 2021
+*/
+
+const fs = require('fs');
+const readline = require('readline');
+
+// accumulator object for keeping data while processing the input file
+const acc = {
+    state: [], // array containing the last to number read
+    pos: 0, // the position in this.state of the last number read
+    shift: function () {this.pos = (this.pos + 1) % 2;},
+    get cur() {
+	return this.state[this.pos];
+    },
+    get other() {
+	return this.state[(this.pos+1)%2];
+    },
+    set cur(val) {
+	this.state[this.pos] = val;
+	return val;
+    }
+};
+
+let counter = 0; // a counter of the number of time depth increase
+
+/* The function for consuming the input file for solving the first
+   problem, this function acts upon each line read on the input file */
+const loop = function(acc, line) {
+    let num = Number(line);
+    acc.shift();
+    acc.cur = num;
+    if (acc.cur > acc.other) { // if new depth is greater
+	counter++;
+    }
+    return;
+};
+
+readline.createInterface({input: fs.createReadStream('1DicAoc.txt')}).on(
+    'line',
+    (line) => loop(acc, line)
+).on(
+    'close',
+    function () {
+	console.log(`The final position of the submarine is ${acc.toString()}`);
+	console.log(`The solution is ${counter}`);
+	return; 
+    }
+);
+
+// Here the solution to the second part
+
+let counter2 = 0;
+
+const acc2 = {
+    state: [], // array containing the last to number read
+    pos: -1, // the position in this.state of the last number read
+    lastTriple : 0,
+    shift: function () {this.pos = (this.pos + 1) % 3;},
+    get cur() {
+	return this.state[this.pos];
+    },
+    get other1() {
+	return this.state[(this.pos+1)%3];
+    },
+    get other2() {
+	return this.state[(this.pos+2)%3];
+    },
+    set cur(val) {
+	this.state[this.pos] = val;
+	return val;
+    }
+};
+
+const summer = (arr) => arr.reduce((x,y) => x+y);
+
+const loop2 = function(acc, line) {
+    let num = Number(line);
+    acc.shift();
+    acc.cur = num;
+    if (acc.state.length === 3) { // if new depth is greater
+	if (acc.lastTriple === 0) {
+	    acc.lastTriple = summer(acc.state);
+	}
+	else {
+	    let newTriple = summer(acc.state);
+	    if (newTriple > acc.lastTriple)
+		counter2++;
+	    acc.lastTriple = newTriple;
+	}
+    }
+    return;
+};
+
+
+readline.createInterface({input: fs.createReadStream('1DicAoc.txt')}).on(
+    'line',
+    (line) => loop2(acc2, line)
+).on(
+    'close',
+    function () {
+	console.log(`The final position of the submarine is ${acc2.toString()}`);
+	console.log(`The solution is ${counter2}`);
+	return; 
+    }
+);
+
---
+++ ecmascript/2DicAoc.js
@@ -0,0 +1,87 @@
+/* 
+* Solutions to the problems of the advent of code 2021
+* 2 Dec 2021
+*/
+const fs = require('fs');
+const rl = require('readline');
+
+
+const submarine = {
+    x: 0,
+    y: 0
+};
+const cmdSubMar = function (sbm, cmd) {
+    let data = null;
+    if ( (data = /forward (?<fdir>[0-9]+)/.exec(cmd)) !== null) {
+	sbm.x += Number(data.groups.fdir);
+    }
+    else if ((data = /up (?<udir>[0-9]+)/.exec(cmd)) !== null) {
+	sbm.y -= Number(data.groups.udir);
+    }
+    else if ((data = /down (?<ddir>[0-9]+)/.exec(cmd)) !== null) {
+	sbm.y += Number(data.groups.ddir);
+    }
+    else { //Something really went wrong
+	console.log(`Invalid input: ${cmd} is an unexpected input\n`);
+    }
+    return ;
+};
+
+const submarine2 = {
+    x: 0,
+    y: 0,
+    aim: 0
+};
+
+const cmdSubMar2 = function (sbm, cmd) {
+    let data = null;
+    if ( (data = /forward (?<fdir>[0-9]+)/.exec(cmd)) !== null) {
+	let num = Number(data.groups.fdir); 
+	sbm.x += num;
+	sbm.y += sbm.aim * num;
+    }
+    else if ((data = /up (?<udir>[0-9]+)/.exec(cmd)) !== null) {
+	sbm.aim -= Number(data.groups.udir);
+    }
+    else if ((data = /down (?<ddir>[0-9]+)/.exec(cmd)) !== null) {
+	sbm.aim += Number(data.groups.ddir);
+    }
+    else { //Something really went wrong
+	console.log(`Invalid input: ${cmd} is an unexpected input\n`);
+    }
+    return ;
+};
+
+// Computing the first solution:
+
+/* Here we solve the first problem
+* using the cmdSubMar on submarine and on the readline
+* associated to the input file '2DicAoc.txt'
+* and output the wished result
+*/
+rl.createInterface({input: fs.createReadStream('2DicAoc.txt')}).on(
+    'line',
+    (line) => // for each line we do...
+    cmdSubMar(submarine,line)
+).on(
+    'close', // once we have finished reading the input....
+    () => {
+	console.log(`The first problem produce a submarine = ${submarine.toString()}\n`);
+	console.log(`The first solution is ${submarine.x * submarine.y}\n`);
+	return ;
+    }
+);
+
+rl.createInterface({input: fs.createReadStream('2DicAoc.txt')}).on(
+    'line',
+    (line) => // for each line we do...
+    cmdSubMar2(submarine2, line)
+).on(
+    'close',
+    () => {
+	console.log(`The second problem produce a submare = ${submarine2.toString()}\n`);
+	console.log(`The solution to the second problem is ${submarine2.x * submarine2.y}\n`);
+	return ;
+    }
+);
+
